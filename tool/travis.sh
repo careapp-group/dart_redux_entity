4e6aa9f050e507f4dfea410ada42b30a7944eda5
Michael Marner <michael@20papercups.net>
2019-11-07 17:40:50 +1030

    ci: Fix coverage generation maybe?

--- tool/travis.sh
+++ tool/travis.sh
@@ -19,21 +19,23 @@ pub run test --reporter expanded
 OBS_PORT=9292
 echo "Collecting coverage on port $OBS_PORT..."
 
-# Start tests in one VM.
-dart \
-  --enable-vm-service=$OBS_PORT \
-  --pause-isolates-on-exit \
-  test/test_all.dart &
-
 # Run the coverage collector to generate the JSON coverage report.
-collect_coverage \
+pub global run coverage:collect_coverage \
   --port=$OBS_PORT \
   --out=var/coverage.json \
   --wait-paused \
-  --resume-isolates
+  --resume-isolates &
+
+# Start tests in one VM.
+dart \
+  --disable-service-auth-codes
+  --enable-vm-service=$OBS_PORT \
+  --pause-isolates-on-exit \
+  test/test_all.dart
+
 
 echo "Generating LCOV report..."
-format_coverage \
+pub global run coverage:format_coverage \
   --lcov \
   --in=var/coverage.json \
   --out=var/lcov.info \
